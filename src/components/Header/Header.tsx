39c573b040eecbec624b8af7a602e78eb0c3d55b
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-07 21:53:54 +0300

    Replace Group for Button on ButtonPanel

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,24 @@
-import { Box, Button, Group, Text } from '@mantine/core';
+import { Box, Group, Text } from '@mantine/core';
 import { ColorSchemeToggle } from '@/components/ColorSchemeToggle';
+import { ButtonPanel } from '@/components/Header/ButtonPanel';
 
 export const Header = () => (
-    <Box p={15}>
+  <Box p={15}>
     <header>
-        <Group justify="space-between" h="100%">
-            <Group>
-                <Text
-                  size="lg"
-                  fw={900}
-                  variant="gradient"
-                  gradient={{ from: 'violet', to: 'rgba(31, 148, 95, 0.99)', deg: 167 }}
-                >THEME MODE:
-                </Text>
-                <ColorSchemeToggle />
-            </Group>
-            <Group>
-                <Button w={100} color="teal">Log in</Button>
-                <Button w={100}>Sign up</Button>
-            </Group>
+      <Group justify="space-between" h="100%">
+        <Group>
+          <Text
+            size="lg"
+            fw={900}
+            variant="gradient"
+            gradient={{ from: 'violet', to: 'rgba(31, 148, 95, 0.99)', deg: 167 }}
+          >
+            THEME MODE:
+          </Text>
+          <ColorSchemeToggle />
         </Group>
+        <ButtonPanel />
+      </Group>
     </header>
-    </Box>
-   );
+  </Box>
+);
