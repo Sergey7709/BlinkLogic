7fbe53b5f794217975035ccc1fc78d9816f0af63
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-07 23:09:37 +0300

    Edit JSX

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,6 +1,9 @@
 import { AuthForm } from '@/components/AuthForm';
 import { useMutation } from '@tanstack/react-query';
 import { instance } from '@/service/api.config';
+import { Button, Flex, Space, Text } from '@mantine/core';
+import { Link } from 'react-router-dom';
+import { routerPath } from '@/Router/constantsRouter';
 
 export const Registration = () => {
   const { mutateAsync } = useMutation({
@@ -20,8 +23,21 @@ export const Registration = () => {
     mutateAsync(loginData);
   };
   return (
-    <>
+    <Flex w="100%" justify="center" align="center" direction="column">
       <AuthForm title="Sign Up" handleSubmit={handleRegister} />
-    </>
+      <Space h="lg" />
+      <Flex w={300} justify="center">
+        <Button variant="subtle">
+          <Text
+            component={Link}
+            to={routerPath.login}
+            variant="gradient"
+            gradient={{ from: 'indigo', to: 'red', deg: 166 }}
+          >
+            Go back
+          </Text>
+        </Button>
+      </Flex>
+    </Flex>
   );
 };
