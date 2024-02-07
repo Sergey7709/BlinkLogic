b04ea9e88ac438c635f7f1fb1c87c5d7368df9a8
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-07 12:02:10 +0300

    Delete baseUrl variable

--- src/components/Table/constants.ts
+++ src/components/Table/constants.ts
@@ -2,19 +2,17 @@ import { MRT_ColumnDef } from 'mantine-react-table';
 import { ColumnsData } from './types';
 
 export const dataColumns: MRT_ColumnDef<ColumnsData>[] = [
-    {
-        accessorKey: 'short',
-        header: 'Short Link',
-    },
+  {
+    accessorKey: 'short',
+    header: 'Short Link',
+  },
 
-    {
-        accessorKey: 'target',
-        header: 'Target Link',
-    },
-    {
-        accessorKey: 'counter',
-        header: 'Counter',
-    },
-  ];
-
-export const baseUrl = 'https://front-test.hex.team';
+  {
+    accessorKey: 'target',
+    header: 'Target Link',
+  },
+  {
+    accessorKey: 'counter',
+    header: 'Counter',
+  },
+];
