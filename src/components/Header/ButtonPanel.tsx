5f6d1fc1cef9f2cf45ddced5c512e0abe89c8ba1
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-07 23:34:32 +0300

    Add useMediaQuery

--- src/components/Header/ButtonPanel.tsx
+++ src/components/Header/ButtonPanel.tsx
@@ -1,18 +1,22 @@
 import { Button, Group } from '@mantine/core';
 import { useAuth } from '@/context';
+import { useMediaQuery } from '@mantine/hooks';
 
 export const ButtonPanel = () => {
   const { isAuthenticated, setIsAuthenticated } = useAuth();
 
+  const matches = useMediaQuery('(max-width: 30rem)');
+
+  console.log(matches);
   const handleLogout = () => {
     localStorage.removeItem('token');
     setIsAuthenticated(false);
   };
 
   return (
-    <Group>
+    <Group w={matches ? '100%' : '6rem'}>
       {isAuthenticated && (
-        <Button w={100} color="rgba(242, 99, 99, 1)" size="sm" onClick={handleLogout}>
+        <Button color="rgba(242, 99, 99, 1)" size="xs" onClick={handleLogout} fullWidth>
           Logout
         </Button>
       )}
