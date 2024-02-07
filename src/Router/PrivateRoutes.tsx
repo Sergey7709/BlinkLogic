e098529fd610fd8efa9237c68e063b1b34682481
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-07 12:04:26 +0300

    Delete comments

--- src/Router/PrivateRoutes.tsx
+++ src/Router/PrivateRoutes.tsx
@@ -1,14 +1,8 @@
-import { memo } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { routerPath } from '@/Router/constantsRouter';
 
-export const PrivateRoutes = memo(() => {
-    // const { isSuccess: isAuthenticated, isLoading } = useGetAuthUserMeDataQuery();
-    const isAuthenticated = true;
+export const PrivateRoutes = () => {
+  const isAuthenticated = false; //!!!
 
-    // if (!isAuthenticated) {
-    //     return <div>Load</div>;
-    // }
-
-    return isAuthenticated ? <Outlet /> : <Navigate to={routerPath.login} />;
-});
+  return isAuthenticated ? <Outlet /> : <Navigate to={routerPath.login} />;
+};
