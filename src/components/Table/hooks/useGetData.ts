89895349f7ddd6d4e75141eb6825318718a465f3
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-07 21:58:36 +0300

    Edited creation logic fetchURL and queryKey in useQuery

--- src/components/Table/hooks/useGetData.ts
+++ src/components/Table/hooks/useGetData.ts
@@ -4,16 +4,22 @@ import { instance } from '@/service/api.config';
 import { DataApiResponse, Params } from '../types';
 
 export const useGetData = ({ sorting, pagination }: Params) => {
-  const fetchURL = new URL(baseUrl);
+  const fetchURL = new URL(`${baseUrl}/api/statistics?`);
+  const transformedSorting = sorting.map((item) => {
+    const direction = item.desc ? 'desc_' : 'asc_';
+    return `${direction}${item.id}`;
+  });
+  transformedSorting.forEach((item) => {
+    fetchURL.searchParams.append('order', item);
+  });
   fetchURL.searchParams.set('offset', `${pagination.pageIndex * pagination.pageSize}`);
   fetchURL.searchParams.set('limit', `${pagination.pageSize}`);
-  fetchURL.searchParams.set('sorting', JSON.stringify(sorting ?? [])); //!!!
 
   return useQuery<DataApiResponse>({
-    queryKey: [['link'], fetchURL.href], //!!!
-    queryFn: () => instance.get(fetchURL.href).then((res) => res.data), //!!!
+    queryKey: ['link', fetchURL.href],
+    queryFn: () => instance.get(fetchURL.href).then((res) => res.data),
     placeholderData: keepPreviousData,
-    // staleTime: 30_000,
+    staleTime: 30_000,
     retry: 1,
   });
 };
