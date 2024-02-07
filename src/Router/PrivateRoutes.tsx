845310e4d5375daaaedf3da9f0dc66aabf8ce856
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-07 16:20:17 +0300

    Add useAuth for isAuthenticated

--- src/Router/PrivateRoutes.tsx
+++ src/Router/PrivateRoutes.tsx
@@ -1,8 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { routerPath } from '@/Router/constantsRouter';
+import { useAuth } from '@/context';
+import { memo } from 'react';
 
-export const PrivateRoutes = () => {
-  const isAuthenticated = false; //!!!
+export const PrivateRoutes = memo(() => {
+  const { isAuthenticated } = useAuth();
+
+  console.log(isAuthenticated, 'render private routes');
 
   return isAuthenticated ? <Outlet /> : <Navigate to={routerPath.login} />;
-};
+});
