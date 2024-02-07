1a0a4cdc137935659f11e31a1dca6c80f7336034
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-07 12:01:44 +0300

    Edit AuthForm, replace name field on username and add PasswordInput

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,52 +1,66 @@
 import { useForm } from '@mantine/form';
-import { Box, Button, Card, Center, Space, TextInput, Title } from '@mantine/core';
+import { Box, Button, Card, Center, PasswordInput, Space, TextInput, Title } from '@mantine/core';
 
 type AuthFormProps = {
-   title: string
+  title: string;
+  handleSubmit: (loginData: { username: string; password: string }) => void;
 };
-export const AuthForm = ({ title }:AuthFormProps) => {
-    const form = useForm({
-        initialValues: { name: '', password: '' },
+export const AuthForm = ({ title, handleSubmit }: AuthFormProps) => {
+  const form = useForm({
+    initialValues: { username: '', password: '' },
 
-        validate: {
-            name: (value:string) => {
-                if (value.length < 3) { return 'Name must have at least 3 letters'; }
-                if (value.length > 6) { return 'Name must not exceed 30 characters'; }
-                return null;
-                    },
-            password: (value: string) => {
-                if (value.length < 6) { return 'Password must have at least 6 characters'; }
-                if (value.length > 30) { return 'Password must not exceed 30 characters'; }
-                return null;
-            },
-        },
-    });
+    validate: {
+      username: (value: string) => {
+        if (value.length < 3) {
+          return 'Name must have at least 3 letters';
+        }
+        if (value.length > 10) {
+          return 'Name must not exceed 10 characters';
+        }
+        return null;
+      },
+      password: (value: string) => {
+        if (value.length < 6) {
+          return 'Password must have at least 6 characters';
+        }
+        if (value.length > 30) {
+          return 'Password must not exceed 30 characters';
+        }
+        return null;
+      },
+    },
+  });
 
-    return (
-        <>
-            <Space h={300} />
-            <Box maw={340} mx="auto">
-                <Card withBorder radius="md" shadow="xl">
-                    <Center>
-                    <Title order={3}>{title}</Title>
-                    </Center>
-                    <Space h={20} />
-                    <form onSubmit={form.onSubmit(console.log)}>
-                        <TextInput label="Name" placeholder="Name" {...form.getInputProps('name')} />
-                        <TextInput mt="sm" label="Password" placeholder="Password" {...form.getInputProps('password')} />
-                        <Space h={20} />
-                        <Button
-                          type="submit"
-                          mt="sm"
-                          variant="gradient"
-                          gradient={{ from: 'teal', to: 'violet', deg: 142 }}
-                          fullWidth
-                        >
-                            Submit
-                        </Button>
-                    </form>
-                </Card>
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Space h={300} />
+      <Box maw={340} mx="auto">
+        <Card withBorder radius="md" shadow="xl">
+          <Center>
+            <Title order={3}>{title}</Title>
+          </Center>
+          <Space h={20} />
+          <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+            <TextInput label="Name" placeholder="Name" {...form.getInputProps('username')} />
+            <PasswordInput
+              mt="sm"
+              label="Password"
+              placeholder="Password"
+              {...form.getInputProps('password')}
+            />
+            <Space h={20} />
+            <Button
+              type="submit"
+              mt="sm"
+              variant="gradient"
+              gradient={{ from: 'teal', to: 'violet', deg: 142 }}
+              fullWidth
+            >
+              Submit
+            </Button>
+          </form>
+        </Card>
+      </Box>
+    </>
+  );
 };
