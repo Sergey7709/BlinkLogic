f71ef65eee267b9cb09ca45f6e1138d719c8c1c1
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-07 12:06:20 +0300

    Edit useQuery

--- src/components/Table/hooks/useGetData.ts
+++ src/components/Table/hooks/useGetData.ts
@@ -1,18 +1,19 @@
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { baseUrl } from '@/service/constantsApi';
+import { instance } from '@/service/api.config';
 import { DataApiResponse, Params } from '../types';
-import { baseUrl } from '../constants';
 
 export const useGetData = ({ sorting, pagination }: Params) => {
   const fetchURL = new URL(baseUrl);
-
   fetchURL.searchParams.set('offset', `${pagination.pageIndex * pagination.pageSize}`);
   fetchURL.searchParams.set('limit', `${pagination.pageSize}`);
   fetchURL.searchParams.set('sorting', JSON.stringify(sorting ?? [])); //!!!
 
   return useQuery<DataApiResponse>({
-    queryKey: ['link', fetchURL.href], //!!!
-    queryFn: () => fetch(fetchURL.href).then((res) => res.json()), //!!!
+    queryKey: [['link'], fetchURL.href], //!!!
+    queryFn: () => instance.get(fetchURL.href).then((res) => res.data), //!!!
     placeholderData: keepPreviousData,
-    staleTime: 30_000,
+    // staleTime: 30_000,
+    retry: 1,
   });
 };
