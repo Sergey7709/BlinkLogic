5a4d20b8c2c97bd79a5d3eed04907eada8065f76
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-07 12:04:45 +0300

    Add useMutation query and add handleSubmit prop for AuthForm

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,6 +1,27 @@
 import { AuthForm } from '@/components/AuthForm';
+import { useMutation } from '@tanstack/react-query';
+import { instance } from '@/service/api.config';
 
-export const Registration = () =>
+export const Registration = () => {
+  const { mutateAsync } = useMutation({
+    mutationFn: async (loginData: { username: string; password: string }) => {
+      try {
+        await instance.post(
+          `/api/register?username=${loginData.username}&password=${loginData.password}`,
+          {}
+        );
+      } catch {
+        console.log('error');
+      }
+    },
+  });
+
+  const handleRegister = (loginData: { username: string; password: string }) => {
+    mutateAsync(loginData);
+  };
+  return (
     <>
-    <AuthForm title="Sign Up" />
-    </>;
+      <AuthForm title="Sign Up" handleSubmit={handleRegister} />
+    </>
+  );
+};
