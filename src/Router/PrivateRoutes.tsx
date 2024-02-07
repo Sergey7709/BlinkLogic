4769954dc2239e95f335a2676fa132862adf3792
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-07 21:55:43 +0300

    Delete memo

--- src/Router/PrivateRoutes.tsx
+++ src/Router/PrivateRoutes.tsx
@@ -1,12 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { routerPath } from '@/Router/constantsRouter';
 import { useAuth } from '@/context';
-import { memo } from 'react';
 
-export const PrivateRoutes = memo(() => {
+export const PrivateRoutes = () => {
   const { isAuthenticated } = useAuth();
 
   console.log(isAuthenticated, 'render private routes');
 
   return isAuthenticated ? <Outlet /> : <Navigate to={routerPath.login} />;
-});
+};
