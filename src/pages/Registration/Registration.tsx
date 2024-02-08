8e167d07e3adafd1c71a88080946c9e89948a1a7
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-08 23:10:19 +0300

    Requests have been moved to the useCreateAccount hook. Add func handleAxiosError. Add errorData variable for JSX.

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,30 +1,28 @@
 import { AuthForm } from '@/components/AuthForm';
-import { useMutation } from '@tanstack/react-query';
-import { instance } from '@/service/api.config';
 import { Button, Flex, Space, Text } from '@mantine/core';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { routerPath } from '@/Router/constantsRouter';
+import { useCreateAccount } from '@/service/hooks/useCreateAccount';
+import { handleAxiosError } from '@/service/function/handleAxiosError';
 
 export const Registration = () => {
-  const { mutateAsync } = useMutation({
-    mutationFn: async (loginData: { username: string; password: string }) => {
-      try {
-        await instance.post(
-          `/api/register?username=${loginData.username}&password=${loginData.password}`,
-          {}
-        );
-      } catch {
-        console.log('error');
-      }
-    },
-  });
+  const { mutateAsync, error, isPending } = useCreateAccount();
+  const navigate = useNavigate();
 
-  const handleRegister = (loginData: { username: string; password: string }) => {
-    mutateAsync(loginData);
+  const errorData = handleAxiosError(error);
+
+  const handleRegister = async (loginData: { username: string; password: string }) => {
+    await mutateAsync(loginData).then(() => navigate(routerPath.login));
   };
+
   return (
     <Flex w="100%" justify="center" align="center" direction="column">
-      <AuthForm title="Sign Up" handleSubmit={handleRegister} />
+      <AuthForm title="Sign Up" handleSubmit={handleRegister} isPending={isPending} />
+      {error && (
+        <Text c="red" size="lg">
+          {errorData?.detail}
+        </Text>
+      )}
       <Space h="lg" />
       <Flex w={300} justify="center">
         <Button variant="subtle">
