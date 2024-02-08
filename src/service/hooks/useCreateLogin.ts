9d55807c3543d6b157ece03d4bf743fb92130c21
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-08 23:12:02 +0300

    Edit UseCreateLogin delete try-catch block

--- src/service/hooks/useCreateLogin.ts
+++ src/service/hooks/useCreateLogin.ts
@@ -13,13 +13,10 @@ type CreateLogin = Pick<AuthContextType, 'setIsAuthenticated'>;
 export const UseCreateLogin = ({ setIsAuthenticated }: CreateLogin) =>
   useMutation({
     mutationFn: async (loginData: { username: string; password: string }) => {
-      try {
-        const resp: LoginRequest = await instance.post('/api/login', loginData);
-        localStorage.setItem('token', resp.data.access_token);
-        const token = localStorage.getItem('token');
-        setIsAuthenticated(!!token);
-      } catch {
-        console.log('error'); //!!!
-      }
+      const resp: LoginRequest = await instance.post('/api/login', loginData);
+      localStorage.setItem('token', resp.data.access_token);
+      const token = localStorage.getItem('token');
+      setIsAuthenticated(!!token);
     },
+    retry: 0,
   });
