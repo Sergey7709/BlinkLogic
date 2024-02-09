cad633954cb9b7913ff5fb8fb6f1fe12c4ce83cb
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-09 11:25:36 +0300

    Edit dataColumns, add accessorKey: 'readyShort'

--- src/components/Table/constants.ts
+++ src/components/Table/constants.ts
@@ -3,13 +3,24 @@ import { ColumnsData } from './types';
 
 export const dataColumns: MRT_ColumnDef<ColumnsData>[] = [
   {
-    accessorKey: 'short',
-    header: 'Short Link',
+    accessorKey: 'id',
+    header: 'ID',
+    enableClickToCopy: false,
+    enableSorting: false,
   },
   {
     accessorKey: 'target',
     header: 'Target Link',
     enableClickToCopy: false,
+  },
+  {
+    accessorKey: 'short',
+    header: 'Short Code',
+  },
+  {
+    accessorKey: 'readyShort',
+    header: 'Short Link',
+    enableSorting: false,
   },
   {
     accessorKey: 'counter',
