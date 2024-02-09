e851a58e1fd010d1d65e1b53891cad2da0499264
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-09 11:27:30 +0300

    Edit ColumnsData, add prop readyShort

--- src/components/Table/types.ts
+++ src/components/Table/types.ts
@@ -1,9 +1,11 @@
 import type { MRT_PaginationState, MRT_SortingState } from 'mantine-react-table';
 
 export type ColumnsData = {
+  id: string;
   short: string;
   target: string;
   counter: string;
+  readyShort: string;
 };
 
 export type Params = {
