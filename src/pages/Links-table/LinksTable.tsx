77a551cef6a731dd7c7bee1b5e0f955bbfd0b9c1
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-09 13:20:37 +0300

    Add ShortLinkCreator component

--- src/pages/Links-table/LinksTable.tsx
+++ src/pages/Links-table/LinksTable.tsx
@@ -1,19 +1,24 @@
-import { Container, Flex } from '@mantine/core';
+import { Box, Container, Flex, Space } from '@mantine/core';
 
 import { Table } from '@/components/Table/Table';
+import { ShortLinkCreator } from '@/components/ShortLinkCreator/ShortLinkCreator';
 
 export function LinksTable() {
   return (
     <>
-        <Container p={10} mt={100}>
+      <Container p={10} mt={100}>
         <Flex
           direction={{ base: 'column' }}
           gap={{ base: 'sm', sm: 'lg' }}
           justify={{ sm: 'center' }}
         >
-      <Table />
+          <Box>
+            <ShortLinkCreator />
+            <Space h={20} />
+          </Box>
+          <Table />
         </Flex>
-        </Container>
+      </Container>
     </>
   );
 }
